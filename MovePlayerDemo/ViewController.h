078208d56512cd60b145ef5e3ae096c0bc4ded19
朱飞飞 <13121668803@163.com>
2015-09-02 14:37:28 +0800

    Initial Commit

---
+++ MovePlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovePlayerDemo
+//
+//  Created by 朱飞飞 on 15/9/2.
+//  Copyright (c) 2015年 朱飞飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovePlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovePlayerDemo
+//
+//  Created by 朱飞飞 on 15/9/2.
+//  Copyright (c) 2015年 朱飞飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
